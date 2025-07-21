2718e3c58bdaf0f4c24ed07eb41917af64ddcebd
simonjoseph <simaojose1978@gmail.com>
2025-07-21 21:19:47 +0100

    set a database

--- src/main/java/com/uevo/Fridge/controller/FoodController.java
+++ src/main/java/com/uevo/Fridge/controller/FoodController.java
@@ -2,8 +2,9 @@ package com.uevo.Fridge.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +18,18 @@ public class FoodController {
     // Injeção de dependência do FoodService
     private final FoodService foodService;
 
-    // Construtor para injeção de dependência
+    // Construtor para injeção de dependência ou Autowired
+    // @Autowired é opcional se houver apenas um construtor
+    // e o Spring irá injetar automaticamente a dependência.
     public FoodController(FoodService foodService) {
         this.foodService = foodService;
     }
 
     @GetMapping
-    public List<Food> getAll() {
-        return foodService.getAll();
+    public List<Food> getAll() {return foodService.getAll();}
+
+    @PostMapping
+    public Food create(@RequestBody Food food) {
+        return foodService.save(food);
     }
 }
